c29c9b6e37c82d5ec4f737ea33b7d17edc6bb4c2
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-16 21:34:33 +0000

    Created an append method to linkedlist
    This adds a new node to the end of the list

---
+++ index.js
@@ -0,0 +1,16 @@
+import { List } from "./list.js";
+
+let list = new List;
+
+console.log(list);
+let x = 12;
+let y = 5;
+let z = 100;
+let j = 1;
+
+list.append(x);
+list.append(y);
+list.append(z);
+list.append(j);
+
+console.log(list);
--- list.js
+++ list.js
@@ -1,5 +1,23 @@
+import { Node } from "./node.js";
+
 export class List {
     constructor(head = null){
         this.head = head;
+    }
+    append(value){
+        //create a new node
+        let node = new Node(value);
+
+        if (this.head == null){
+            this.head = node;
+        } else {
+            let currentNode = this.head
+
+            while (currentNode.next !== null){
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = node;
+        }
     }
 }
